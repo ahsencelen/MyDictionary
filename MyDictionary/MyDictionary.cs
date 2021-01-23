6dace1ccd0ddcbfe59b1ee276d0006869f39e88d
ahsencelen <ahsencln@gmail.com>
2021-01-23 19:00:30 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1, T2, T3>
+    {
+        T1[] Ids;
+        T2[] names;
+        T3[] jobs;
+
+        public MyDictionary()
+        {
+            Ids = new T1[0];
+            names = new T2[0];
+            jobs = new T3[0];
+        }
+        public void Add(T1 Id, T2 name, T3 job)
+        {
+            T1[] tempArray = Ids;
+            T2[] tempArray2 = names;
+            T3[] tempArray3 = jobs;
+
+            Ids = new T1[Ids.Length + 1];
+            names = new T2[names.Length + 1];
+            jobs = new T3[jobs.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                Ids[i] = tempArray[i];
+                names[i] = tempArray2[i];
+                jobs[i] = tempArray3[i];
+            }
+
+            Ids[Ids.Length - 1] = Id;
+            names[names.Length - 1] = name;
+            jobs[jobs.Length - 1] = job;
+        }
+        public int Length
+        {
+            get { return jobs.Length; }
+        }
+        public T1[] Id
+        {
+            get { return Ids; }
+        }
+        public T2[] Name
+        {
+            get { return names; }
+        }
+        public T3[] Job
+        {
+            get { return jobs; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string, string> members = new MyDictionary<int, string, string>();
+            members.Add(123456, "Ahsen Çelen", "Engineer");
+            members.Add(456789, "Anıl Ateş", "Doctor");
+            members.Add(789456, "Elif Topaç", "Teacher");
+            members.Add(456123, "Furkan Küçük", "Engineer");
+
+            Console.WriteLine("total number of members: " + members.Length);
+
+            Console.WriteLine("--- Members ID cards ---");
+            foreach (var Id in members.Id)
+            {
+                Console.WriteLine(Id);
+            }
+            Console.WriteLine("-----Members Name----");
+            foreach (var name in members.Name)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("----Members Job-----");
+            foreach (var jobs in members.Job)
+            {
+                Console.WriteLine(jobs);
+            }
+
+        }
+    }
+}
